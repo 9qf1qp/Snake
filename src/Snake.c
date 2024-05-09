f8a855f008b4eccbcdad5608f0601f8e6e75f752
9qf1qp <9qf1qp@proton.me>
2024-05-08 18:24:38 -0700

    OK

--- src/Snake.c
+++ src/Snake.c
@@ -1,24 +1,9 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+#include<Windows.h>
 int i,j,height=20,width=20,gameover,score;
 int x,y,fruit,fruity,fruitx,flag;
-
-void draw()
-{
-  for (i=0;i<height;i++){
-    for (j = 0; j<width; j++){
-      if (i==0 || i == width -1 || j == 0 || j == height -1) {
-        printf("#");
-      }
-      else {
-        printf(" ");
-      }
-    }
-    printf("\n");
-  }
-}
-
 
 /*Using rand()%20 because the size of the boundary is 
 length = 20 and width = 20 so the fruit will generate
@@ -38,6 +23,35 @@ void setup()
     if (fruity==0)
       goto label2;
     score=0;
+}
+
+void draw() 
+{ 
+    system("cls"); 
+    for (i = 0; i < height; i++) { 
+        for (j = 0; j < width; j++) { 
+            if (i == 0 || i == width - 1 || j == 0 || j == height - 1) { 
+                printf("#"); 
+            } 
+            else { 
+                if (i == x && j == y) {
+                    printf("0");
+                }
+                else if (i == fruitx && j == fruity) {
+                    printf("*"); 
+                }
+                else{
+                    printf(" ");
+            } 
+            } 
+        } 
+        printf("\n"); 
+    } 
+        // Print the score after the 
+    // game ends 
+    printf("score = %d", score); 
+    printf("\n"); 
+    printf("press X to quit the game"); 
 }
 
 void input ()
@@ -66,40 +80,52 @@ void input ()
 }
 
 void logic() 
-{
-  sleep(0.01);
-  switch(flag)
-  {
-    case 1:
-      y--;
-      break;
-    case 2:
-      x++;
-      break;
-    case 3:
-      y++;
-      break;
-    case 4:
-      x--;
-      break;
-    default:
-      break;
-  }
-  if(x<0 || x>height || y<0 || y>width){
-    gameover=1;
-  }
-  if(x==fruitx&&y==fruity){
-        label4:
-      fruity=rand()%20;
-      if (fruity==0){
-        goto label4;
-      }
-    score+=10;
-    }
-}
+{ 
+    Sleep(0.01); 
+    switch (flag) { 
+    case 1: 
+        y--; 
+        break; 
+    case 2: 
+        x++; 
+        break; 
+    case 3: 
+        y++; 
+        break; 
+    case 4: 
+        x--; 
+        break; 
+    default: 
+        break; 
+    } 
+  
+    // If the game is over 
+    if (x < 0 || x > height 
+        || y < 0 || y > width) 
+        gameover = 1; 
+  
+    // If snake reaches the fruit 
+    // then update the score 
+    if (x == fruitx && y == fruity) { 
+    label3: 
+        fruitx = rand() % 20; 
+        if (fruitx == 0) 
+            goto label3; 
+  
+    // After eating the above fruit 
+    // generate new fruit 
+    label4: 
+        fruity = rand() % 20; 
+        if (fruity == 0) 
+            goto label4; 
+        score += 10; 
+    } 
+} 
 
 void main() 
 { 
+
+  int m,n;
     // Generate boundary 
     setup(); 
   
